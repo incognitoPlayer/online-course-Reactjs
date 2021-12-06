044f6a303ba9271d31a46769ce1a078ec897718d
rizalfmaulana <rizalfmaulana@gmail.com>
2021-12-06 21:37:04 +0700

    modified input otp

--- src/components/InputOtp.jsx
+++ src/components/InputOtp.jsx
@@ -1,8 +1,8 @@
 const InputOtp = () => {
   return (
     <input
-      class="m-2 flex-1 border h-[100px] w-[100px] text-center rounded text-5xl"
-      type="text"
+      class="m-2 bg-[#FAFAFA] border-[#d8d8d8] flex-1 border h-[100px] w-[100px] text-center text-5xl focus:ring-0 focus:ring-offset-0 rounded-lg focus:ring-primaryColor focus:border-primaryColor"
+      type="number"
       maxlength="1"
     />
   );
